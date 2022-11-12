0f977efa2ac9acf20e03de8c9e0b4218d1660488
Alejandro <alejandroarias0312@gmail.com>
2022-11-12 16:32:52 -0500

    form group added

---
+++ src/app/models/signup.interface.ts
@@ -0,0 +1,16 @@
+
+
+export interface Signup{
+ 
+    firstname: string;
+
+  
+    lastname: string;
+
+
+    birthdate: string;
+
+    username: string;
+
+    password: string;
+}
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    private formBuilder:FormBuilder
+  ) { }
+
+  signupForm = this.formBuilder.group({
+    firstname:new FormControl('',[Validators.required]),
+    lastname: new FormControl('',[Validators.required]),
+    birth: new FormControl('',[Validators.required]),
+    username: new FormControl('',[Validators.required]),
+    pass: new FormControl('',[Validators.required,Validators.minLength(6)]),
+    confPass: new FormControl('',[Validators.required])
+  },
+  { validator: this.matchPassword('pass', 'confPass') });
+
+
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void{
+
+    console.log(this.signupForm);
+  }
+
+  
+  matchPassword(firstControl:any, secondControl:any): ValidatorFn {
+ 
+    return (control: AbstractControl): ValidationErrors | null => {
+ 
+      const password = control.get(firstControl)?.value;
+      const confirm = control.get(secondControl)?.value;
+ 
+      if (password != confirm) { return { 'noMatch': true } }
+ 
+      return null
+ 
+    }
   }
 
 }
